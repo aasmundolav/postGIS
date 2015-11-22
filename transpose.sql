48fee1bda467fc23da1a16d14c8f08ff2f7af68c
aasmundolav <aasmundolav@users.noreply.github.com>
2015-11-22 21:37:57 +0100

    Update transpose.sql

--- transpose.sql
+++ transpose.sql
@@ -1,9 +1,13 @@
 
 
-
 SELECT
   ItemNo
+
 UNNEST(ARRAY['Item1','Item2','Item3','Item4']) AS ItemName
+
 UNNEST(ARRAY[Item1,Item2,Item3,Item4]) AS Amount
+
 FROM tblUnPivotExample
+
 ORDER BY ItemNo
+
